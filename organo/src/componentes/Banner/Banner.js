2087a053e4aa00bd77c202c0254f0d4dd3872073
Mariana Pinto <mariana.vendasbb@gmail.com>
2022-09-22 10:35:19 -0300

    Banner component

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,8 @@
+function Banner() {
+    // JSX => Trabalha com a parte visual.
+ return (
+    <img src="/imagens/banner.png" alt="Banner principal da página doOrganic"></img>
+ )
+}
+
+export default Banner
