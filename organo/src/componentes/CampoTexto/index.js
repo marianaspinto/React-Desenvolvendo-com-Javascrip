8c5f8b43a424959ec6484499b2d96eaa5b5bc11d
Mariana Pinto <mariana.vendasbb@gmail.com>
2022-09-23 14:45:39 -0300

    adding props to form components

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -1,10 +1,13 @@
 import './CampoTexto.css'
 
-const CampoTexto = () => {
+const CampoTexto = (props) => { // Props são as propriedades que o componente recebeu
+
     return (
         <div className="campo-texto">
-            <label>Nome:</label>
-            <input placeholder="Digite o seu nome"/>
+            <label>
+                {props.label}
+            </label>
+            <input placeholder={props.placeholder}/>
         </div>
     )
 }
